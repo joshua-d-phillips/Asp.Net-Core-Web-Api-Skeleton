903312f460f64f96494ac7b9bc1302b7755d5785
Joshua_D_Phillips <joshua_d_phillips@dell.com>
2018-09-06 19:04:16 -0500

    Initital api skeleton that has defined styles with editorconfig and both unit and integration tests.

---
+++ Api.Integration/Functional/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Api.Integration.Functional.Controllers {
+    public class ValuesController {
+
+        public class GetShould : IClassFixture<WebApplicationFactory<Startup>> {
+            private readonly HttpClient _client;
+
+            public GetShould (WebApplicationFactory<Startup> factory) => _client = factory.CreateClient ();
+
+            [Trait ("Category", "Functional")]
+            [Fact (DisplayName = "API - Integration - Functional - ValuesController - Get - Should return array of strings")]
+            public async Task ReturnArrayOfStrings () {
+                async Task<bool> IsValid (HttpResponseMessage message) {
+                    var isValid = true;
+
+                    if (!message.IsSuccessStatusCode) { isValid = false; }
+
+                    var content = await message.Content.ReadAsStringAsync ();
+
+                    var json = new JArray();
+                    try {
+                        json = JArray.Parse (content);
+                    } catch {
+                        isValid = false;
+                    }
+
+                    foreach (var token in json.Children ()) {
+                        if (token.Type != JTokenType.String) { isValid = false; }
+                    }
+
+                    return isValid;
+                }
+
+                //Act
+                var response = _client.GetAsync ("/api/values").Result;
+
+                //Assert
+                Assert.True (await IsValid (response));
+            }
+        }
+    }
+}
---
+++ Api.Unit/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Api.Controllers;
+using Xunit;
+
+namespace Api.Unit.Controllers {
+    public class ValuesControllerTest {
+
+        public class GetShould {
+            [Trait ("Category", "Unit")]
+            [Fact (DisplayName = "API - Unit - ValuesController - Get - Should return a list of string values")]
+            public void ReturnListOfValues () {
+                //Arrange
+                void action (string value) => Assert.IsType<string> (value);
+
+                //Act
+                var actual = new ValuesController ().Get ();
+
+                //Assert
+                Assert.All<string> (actual.Value, action);
+            }
+
+            [Trait ("Category", "Unit")]
+            [Fact (DisplayName = "API - Unit - ValuesController - Get - Should return string value for specified id")]
+            public void ReturnValueForId () {
+                //Act
+                var actual = new ValuesController ().Get (2);
+
+                //Assert
+                Assert.True (actual.Value is string);
+            }
+        }
+    }
+}
